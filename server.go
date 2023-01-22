e8f6da1c61767ecc09f9795aeeb852249491a17d
smarcosm <sebastiaomarcos49@gmail.com>
2023-01-21 22:57:38 -0300

    Atualizando o server

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/index/", indexHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 	http.ListenAndServe(":8080", nil)
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
